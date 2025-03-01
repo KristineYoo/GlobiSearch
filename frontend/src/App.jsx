95f0504c0febae5eb78d4b3d22d85826ff86790b
Iain Gore <irg35@drexel.edu>
2025-03-01 14:02:39 -0500

    button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <LandingPage />
-      <Box  sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', width:1/2}} >
+      <Box  sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
       <Button fullWidth variant="contained" sx={{ borderRadius: 8, justifyContent: 'center', height:70}}><Typewriter strings={texts} speed={100} delay={1500}/></Button>
       </Box>
     </>
