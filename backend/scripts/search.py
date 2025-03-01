b175b09923e89c47ff426ca40c5e31dc7a447d7e
Andrey Barriga <155704913+andreybarriga@users.noreply.github.com>
2025-03-01 13:59:18 -0500

    description logic

--- backend/scripts/search.py
+++ backend/scripts/search.py
@@ -1,16 +1,23 @@
 import os
+import openai
+import requests
+from openai import OpenAI
+from bs4 import BeautifulSoup
 from google.cloud import translate
 from google.cloud import translate_v2 as translate
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
-# load env variables
+# load env variables and openAI client
 load_dotenv()
+client = OpenAI()
+client.api_key = os.environ.get("OPENAI_API_KEY")
 
 GOOGLE_API = os.environ.get('GOOGLE_API')
 SEARCH_ENGINE_ID = os.environ.get('SEARCH_ENGINE_ID')
 PROJECT_ID = os.environ.get('PROJECT_ID')
 PARENT = f"projects/{PROJECT_ID}"
+
 
 def search_user_language(user_search: str, language_code: str="eng") -> list[dict]:
     """
@@ -65,7 +72,90 @@ def translate_text(target: str, text: str) -> dict:
 
     return result
 
+
+def generate_chatgpt_description(potential_descriptions: str, want_response_obj: bool=False) -> str:
+    """
+    ### Use chatgpt-4o-mini API to generate in a different language 
+    website summary/description
+    prompt already hardcoded since we will want the to keep generating a
+    specific format for a website's summary/description.
+
+    #### args:
+    potential_descriptions : the parsed html code of the website
+    that could give the model an idea of what to summarize
+
+    want_response_obj : return full response object from the model's
+    response (default false)
+    """
+
+    prompt = f"""
+            Can you go through the html code of this specific web page 
+            (which maybe in ANY language) and then find the absolute best 
+            description of this web page? Give me the best description with your own 
+            added summary of what this webpage is about in the language of the web page!
+            Make this description/summary always in context of the web page (by keeping
+            the page's terminology and specifics etc..) and 
+            keep it a paragraph's length at max always. Retain 
+            the most important information
+            and give it backed in a nice formatted short 
+            paragraph. here is the html code.
+            
+
+            {potential_descriptions}
+        """
     
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You're a helpful assistant trying to generate a description based off a web page in a specific language"},
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.3,
+        max_tokens=200,
+    )
+
+    # response_obj -> choices entry -> first entry in choices list (choice obj)-> messages entry
+    chat_response = response.choices[0].message.content
+
+    if want_response_obj:
+       print(f"Response Object: {response}")
+    
+    # return the model's text response
+    return chat_response
+
+def get_website_description(url: str) -> str | None:
+    """
+    ### finds a better description for a webpage:
+    uses bs4 to go parse through a web page's html code
+    to get paragraph tags specifically after the instance of an 
+    image to get a a better description of what
+
+    uses chatgpt to parse through the entire website page
+    in the specific language and find a specific entry that provides
+    the clearest description of the website
+
+    try to parse through a link with beautiful soup,
+    if it fails then simply return 'None' indicating no link could be found
+    """
+
+    try:
+        response = requests.get(url)
+        page_html = response.text
+        
+        # this will fail if its specific file format that won't work
+        soup = BeautifulSoup(page_html, "html.parser")
+      
+        potential_descriptions = str(page_html)
+
+        # generate chatgpt description 
+        best_description = generate_chatgpt_description(potential_descriptions)
+
+        return best_description
+    
+    except:
+       None
+
+
 def search_dif_languages(user_search: str, languages: list) -> dict:
     """
     ### translate user's original google search to different language and get results
@@ -94,13 +184,25 @@ def search_dif_languages(user_search: str, languages: list) -> dict:
         }
     return search_data
 
-        
 
-language_codes = ["es", "fr", "ja"] # testing 
+
 search_info = search_user_language("I want to make pizza")
+
 translated_text = translate_text("es", "good morning how do you do")
 
-print(search_dif_languages(user_search="I want to make pizza", languages=language_codes))
+# testing for codes
+language_codes = ["es", "fr", "ja"]
+multilang_search_info = search_dif_languages(user_search="I want to make pizza", languages=language_codes)
+
+for lang, prompt in multilang_search_info.items():
+    print(f"language: {lang}\n{prompt}")
+
+# testing chatgpt response
+example_link = "https://www.myplate.gov/es/recipes/pizza-facil-y-rapida"
+web_description = str(get_website_description(example_link))
+
+print(f"\n\nweb-desc:\n{web_description}\nweb-link:{example_link}")
+
 
 
 
