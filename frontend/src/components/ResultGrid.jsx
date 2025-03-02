de0b62c8ecf313e9b0bfa48b5fef5d8c5a760be9
Andrey Barriga <155704913+andreybarriga@users.noreply.github.com>
2025-03-02 10:56:54 -0500

    resultgrid file changed

--- frontend/src/components/ResultGrid.jsx
+++ frontend/src/components/ResultGrid.jsx
@@ -1,29 +1,60 @@
 import Grid from '@mui/material/Grid2';
 import ResultCard from './ResultCard';
 
+import React, {useState, useEffect} from 'react';
 
-function ResultGrid(){
-    const results={
-        title:"test",
-        lang:"french",
-        snippit:"This is a snippet",
-        link:"https://mui.com/material-ui/react-grid2/?srsltid=AfmBOor3kQQeeqeRT_VWKixPHCtXxTnnbLBxgo4TJ0dZ8D0jqRU49sld"
-    }
+
+function ResultGrid() {
+
+    // add loading state to display loading 
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // then chaining to get response w/ then chaining 
+    useEffect(() => {
+        // get data from our flask api
+        fetch("https://localhost:5000/api/get-search-result")
+        .then(response => {
+                // throw the error if something happens (server side)
+                if (!response.ok) {
+                    throw new Error("Server couldn't send data");
+                }
+                return response.json()
+        })
+        
+        // get data into react state and make loading false
+        .then( data => {
+            setData(data)
+            setLoading(false)
+        })
+
+        // show possible error in server console
+        .catch(error =>{
+            console.error(error)
+        });
+    }, []);
+    
+    // add if statements for loading to display components
+    if (loading) return <p>Loading..</p>;
+
+    if (error) return <p>the error: {error}</p>;
+
+    if (!data) return <p>No data could be found?</p>
 
     return (
+        // get the entire grid and map the results of each dict to frontend
+        // mapping each key to its value
         <Grid container spacing={4} justifyContent="center">
-            <Grid size={4}>
-                <ResultCard info={results}/>
-            </Grid>
-            <Grid size={4}>
-                <ResultCard info={results}/>
-            </Grid>
-            <Grid size={4}>
-                <ResultCard info={results}/>
-            </Grid>
-        </Grid>
+        {data['top-results'].map((result, index) => (
+          <Grid size={4} key={index}>
+            <ResultCard info={result} />
+          </Grid>
+        ))}
+      </Grid>
     )
 }
+    
+
 
 export default ResultGrid
 
